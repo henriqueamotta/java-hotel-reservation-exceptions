d26973e8d0c674dc2c7d27403c63a4acfc8fc263
Henrique Motta <henriqueamotta@gmail.com>
2020-12-17 16:04:15 -0300

    Bad solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -42,9 +42,17 @@ public class Reservation {
 	}
 	
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for update must be future dates.";
+		}
+		if (!checkOut.after(checkIn)){
+			return "Error in reservation: Check-out date must be after check-in date.";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	
@@ -60,10 +68,5 @@ public class Reservation {
 				+ duration()
 				+ " nights.";
 	}
-	
-	
-	
-	
-	
 
 }
